d7068415b1f18b4e496a74ba9dc8f6c1202e6390
Maggie Tamanini <82274835+teiggam@users.noreply.github.com>
2021-06-29 16:54:13 -0400

    Initial commit for movie registration lab

---
+++ MovieRegistration/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Registration()
+        {
+            return View();
+
+        }
+        public IActionResult Results(Movies inputMovie)
+        {
+            return View(inputMovie);
+        }
+
+
+        MovieDbContext db = new MovieDbContext();
+        public IActionResult MovieList()
+        {
+            List<Movies> m = db.Movies.ToList();
+            return View(m);
+        }
+
+        public static void AddMovie(Movies mov, MovieDbContext db)
+        {
+
+            db.Movies.Add(mov);
+            db.SaveChanges();
+        }
+    }
+
+}
---
+++ MovieRegistration/Models/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MovieRegistration.Models
+{
+    public partial class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int? Year { get; set; }
+        public int? Runtime { get; set; }
+    }
+}
